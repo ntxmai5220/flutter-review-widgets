47435fef738960a398ffaf2e2fbba015eec77982
Xuan Mai <xuanmai.k18@gmail.com>
2021-12-08 17:58:16 +0700

    using method channel

--- android/app/src/main/java/com/example/review_widgets/MainActivity.java
+++ android/app/src/main/java/com/example/review_widgets/MainActivity.java
@@ -1,6 +1,61 @@
 package com.example.review_widgets;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+    private static  final String CHANNEL="com.example.review_widgets/battery";
+//     @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        GeneratedPluginRegistrant.registerWith(this);
+//
+//
+//    }
+    private int getBattery() {
+        int batteryLevel = -1;
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+        } else {
+            Intent intent = new ContextWrapper(getApplicationContext()).
+                    registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            batteryLevel = (intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100) /
+                    intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        }
+
+        return batteryLevel;
+    }
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            // Note: this method is invoked on the main thread.
+                            // TODO
+                            if (call.method.equals("getBattery")) {
+                                int batteryLevel = getBattery();
+                                System.out.println(batteryLevel);
+                                if (batteryLevel != -1) {
+                                    result.success(batteryLevel);
+                                } else {
+                                    result.error("UNAVAILABLE", "Battery level not available.", null);
+                                }
+                            } else {
+                                result.notImplemented();
+                            }
+                        }
+                );
+    }
+
+    
 }
