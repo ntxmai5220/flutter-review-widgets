99df47f97a08bb0330f21390f065304cde20c5b6
Xuan Mai <xuanmai.k18@gmail.com>
2021-12-10 13:58:21 +0700

    firebase_crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-import 'widgets/animation/animation.dart';
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables(); //ENVI
+  await Firebase.initializeApp();
+  
   runApp(MyApp());
 }
 
@@ -11,12 +16,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: FlutterConfig.get("APP_NAME"),
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: AnimationPage(),
+      home: NewScreen(),
+    );
+  }
+}
+
+class NewScreen extends StatelessWidget {
+  const NewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    void _firebaseCrash() async {
+      FirebaseCrashlytics.instance.crash();
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+    }
+
+    return Center(
+      child: ElevatedButton(
+        child: Text(FlutterConfig.get("APP_NAME")),
+        onPressed: () {
+          _firebaseCrash();
+        },
+      ),
     );
   }
 }
